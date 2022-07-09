bd459139120259240d23fe83c0198e0cc97f481c
Won-young <dnjsdud2257@gmail.com>
2022-07-10 04:07:25 +0900

    [허원영][프로젝트생성 화면]달력 추가 완료

--- FE/src/App.js
+++ FE/src/App.js
@@ -3,26 +3,28 @@ import SignUp from "./pages/sign/SignUp";
 import SignIn from "./pages/sign/SignIn";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import ProjectSide from "./components/ProjectSide";
-import PlanSideBar from "./components/PlanSideBar";
+import ProjectSide from "./components/sidebar/ProjectSide";
+import PlanSideBar from "./components/sidebar/PlanSideBar";
 import CreateProject from "./components/CreateProject";
+
 import "./App.css";
 import "./reset.css";
 
 function App() {
   return (
     <>
+      <CreateProject />
+      {/* <div style={{ display: "flex" }}>
+        <ProjectSide />
+        <PlanSideBar />
+      </div> */}
+
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="signin/*" element={<SignIn />} />
           <Route path="signup/*" element={<SignUp />} />
         </Routes>
-      </Router>
-      <Router>
-        <Home />
-        <SignIn />
-        <SignUp />
       </Router>
     </>
   );
