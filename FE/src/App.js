e54f5705570ea300dc6c4143e67a666c73e63535
dbsgur <kkr431@naver.com>
2022-07-09 23:28:41 +0900

    [feat] hyeok router

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import SignUp from "./pages/sign/SignUp";
 import SignIn from "./pages/sign/SignIn";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <SignIn />
-      <SignUp />
-    </>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="signin/*" element={<SignIn />} />
+        <Route path="signup/*" element={<SignUp />} />
+      </Routes>
+    </Router>
+    // <Router>
+    //   <Home />
+    //   <SignIn />
+    //   <SignUp />
+    // </Router>
   );
 }
 
