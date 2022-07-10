49a7f65d68033df8fd91ee770a07d3f61d77a5a1
Suyeon-B <tndus4243@naver.com>
2022-07-11 05:56:43 +0900

    [수연][share-memo] Implement quill & collaborative editor

--- FE/src/App.js
+++ FE/src/App.js
@@ -3,8 +3,9 @@ import SignUp from "./pages/sign/SignUp";
 import SignIn from "./pages/sign/SignIn";
 import SearchMap from "./pages/search/Search";
 import Home from "./pages/Home";
-import Editor from "./pages/shareMemo/shareMemo";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+// import Editor from "./pages/shareMemo/shareMemo";
+import TextEditor from "./pages/shareMemo/TextEditor";
+import { BrowserRouter as Router, Switch, Routes, Route, Navigate } from "react-router-dom";
 import ProjectSide from "./components/sidebar/ProjectSide";
 import PlanSideBar from "./components/sidebar/PlanSideBar";
 import CreateProject from "./components/CreateProject";
@@ -13,18 +14,24 @@ import "./App.css";
 import "./reset.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 
+// [수연][TextEditor] socket io 작업
+import { v4 as uuidV4 } from "uuid";
+
 const queryClient = new QueryClient(); // 인스턴스 생성
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Router>
-        {/* <ProjectSide /> */}
+        <ProjectSide />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="signin/*" element={<SignIn />} />
           <Route path="signup/*" element={<SignUp />} />
-          <Route path="shareMemo/*" element={<Editor />} />
+          {/* <Route path="shareMemo/*" element={<TextEditor />} /> */}
+          {/* [수연][TextEditor] line 33 -> 추후 projectID 기준으로 변경 예정 */}
+          <Route path="shareMemo/*" element={<Navigate to={`/shareMemo/${uuidV4()}`} replace />} />
+          <Route path="/shareMemo/:id" element={<TextEditor />} />
           <Route path="project/*" element={<CreateProject />} />
           <Route path="search/*" element={<SearchMap />} />
           <Route path="*" element={<Navigate to="/" replace />} />
