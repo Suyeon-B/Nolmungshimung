4b996d65927b5424672030a453b65ba027a4b3a8
Won-young <dnjsdud2257@gmail.com>
2022-07-10 11:33:06 +0900

    [허원영][project] project 생성 api 및 DB설계

---
+++ BE/models/Project.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const projectSchema = mongoose.Schema({
+  // object_id: mongoose.Schema.Types.object_id,
+  start_date: { type: Array, required: true },
+  end_date: { type: Array, required: true },
+  term: { type: Number },
+  project_title: { type: String, required: true },
+  people: { type: Array },
+  routes: { type: Array },
+  memo: { type: Array },
+  log: { type: Array },
+  upload_flag: { type: Array },
+});
+
+module.exports = mongoose.model("Project", projectSchema);
