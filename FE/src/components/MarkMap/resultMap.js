dfea3de8f5cc1f710a2d0347f6f6adc53872de3d
youngji <youngji.jang804@gmail.com>
2022-07-23 14:19:47 +0900

    [영지][지도] 결과 페이지 지도 추가

--- FE/src/components/MarkMap/resultMap.js
+++ FE/src/components/MarkMap/resultMap.js
@@ -39,7 +39,7 @@ function ResultMap(props) {
     var mapContainer = document.getElementById("map"), // 지도를 표시할 div
       mapOption = {
         center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-        level: 5, // 지도의 확대 레벨
+        level: 4, // 지도의 확대 레벨
       };
 
     // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
