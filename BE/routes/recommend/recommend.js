666b8f5bf5c8eea910f9a7bade0361f656c6b738
Suyeon-B <tndus4243@naver.com>
2022-07-24 18:37:06 +0900

    [수연][recommendPage] uploadedPage data 반영, detail 페이지 연결

--- BE/routes/recommend/recommend.js
+++ BE/routes/recommend/recommend.js
@@ -4,27 +4,10 @@ const UploadProject = require(__base + "models/UploadProject");
 const HashTable = require(__base + "models/HashTable");
 
 router.get("/", async (req, res, next) => {
-  // const body = req.body;
-  // console.log(body);
-  //   console.log(UploadProject.find());
-
   try {
     const uploadProjectInfo = await UploadProject.find();
-    console.log(uploadProjectInfo);
-
-    // for (let i = 0; i < uploadProjectInfo.people.length; i++) {
-    //   if (projectInfo.people[i][0] === body._id) {
-    //     projectInfo.people.splice(i, 1);
-    //   }
-    // }
-
-    // await uploadProjectInfo.save();
-
-    // userInfo.user_projects = userInfo.user_projects.filter((projectId) => projectId !== id);
-
-    // await userInfo.save();
-
-    // return res.status(200).send({ success: "deleteSuccess" });
+    return res.json(uploadProjectInfo);
+    // console.log(uploadProjectInfo);
   } catch (error) {
     console.log(`uploaded project find: ${error}`);
     res.status(404).send({ error: "project not found" });
--- FE/src/pages/recommend/RecommendPage.js
+++ FE/src/pages/recommend/RecommendPage.js
@@ -5,10 +5,13 @@ import { HomeFilled } from "@ant-design/icons";
 import styled from "styled-components";
 
 const ProjectItem = ({ el }) => {
-  console.log("ProjectItem");
+  // console.log("ProjectItem");
   return (
     <Link to={`project/${el._id}`}>
-      <h1 style={{ fontSize: "25px" }}> {el.project_title}</h1>
+      <RecommendItems>
+        <div class="recommend_project_title"> {el.project_title}</div>
+        <div class="recommend_project_hashtag"> #{el.hashTags}</div>
+      </RecommendItems>
     </Link>
   );
 };
@@ -17,14 +20,31 @@ const RecommendPage = () => {
   const navigate = useNavigate();
   const [items, setItems] = useState([]);
   const mainText = "마음에 드는 여행 프로젝트를\n 내 프로젝트로! 😆";
+  let uploadedProjectsInfo = null;
 
   // 업로드된 프로젝트를 가져온다.
   useEffect(() => {
     async function fetchUploadedProjects() {
-      const response = await fetch(`https://${process.env.REACT_APP_SERVER_IP}:8443/recommend`);
-      return response.json();
+      const response = await fetch(`https://${process.env.REACT_APP_SERVER_IP}:8443/recommend`, {
+        method: "get",
+        headers: {
+          "content-type": "application/json",
+        },
+        credentials: "include",
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          // console.log("===== fetch 결과 =====");
+          // console.log(res);
+          setItems(res);
+          // console.log(items);
+        });
+      // setItems(response);
+      // console.log(response.json());
+      // return response.json();
     }
     fetchUploadedProjects();
+    // console.log(items);
   }, []);
 
   return (
@@ -67,9 +87,38 @@ const RecommendBlock = styled.div`
   padding: 8vh;
   letter-spacing: 1px;
   line-height: 65px;
+  min-width: 800px;
 `;
 
-const RecommendContents = styled.div``;
+const RecommendContents = styled.div`
+  display: flex;
+  margin-top: 100px;
+  overflow-y: hidden;
+  ::-webkit-scrollbar {
+    /* width: 0px;
+    height: 7px; */
+    display: none;
+  }
+`;
+
+const RecommendItems = styled.div`
+  height: 200px;
+  width: 200px;
+  border-radius: 10px;
+  background-color: white;
+  margin-right: 20px;
+  .recommend_project_title {
+    font-size: 25px;
+    color: black;
+    text-align: center;
+    padding: 30px;
+  }
+  .recommend_project_hashtag {
+    font-size: 15px;
+    color: black;
+    text-align: center;
+  }
+`;
 
 const RecommendHome = styled(HomeFilled)`
   color: #ff8a3d;
