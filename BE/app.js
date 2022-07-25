50496d31e40b53570c0c58ab0796933663addf53
박준규 <wnsrb003@naver.com>
2022-07-26 01:17:55 +0900

    [규][도커][auth관련 쿠키 전달을 위해 credential를 사용할 때, 서버의 cors에서 origin에 직접적인 url추가하여야 하므로 추가]

--- BE/app.js
+++ BE/app.js
@@ -215,7 +215,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(cors({ credentials: true, origin: true })); //credential은 프론트엔드의 fetch를 통해서 cookie를 넘기기 위해서 사용함. (프론트엔드에서는 "credentials:true" 설정 필요)
+app.use(cors({ credentials: true, origin: process.env.REACT_APP_SERVER_IP || true })); //credential은 프론트엔드의 fetch를 통해서 cookie를 넘기기 위해서 사용함. (프론트엔드에서는 "credentials:true" 설정 필요)
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
